843a4fbce1e12fd677002887e2bba490f6ff89aa
EzequielSR <ezequieldesr@gmail.com>
2025-02-09 20:55:41 -0300

    Adição testes de falha em TransferenciaTest

--- src/main/java/app/netlify/bugbank/pages/Transferencia.java
+++ src/main/java/app/netlify/bugbank/pages/Transferencia.java
@@ -45,10 +45,62 @@ public class Transferencia extends FecharModal {
         digitoInput.sendKeys(this.digitoDestino);
 
         WebElement valorInput = driver.findElement(By.name("transferValue"));
-        valorInput.sendKeys("100.0"); // Valor fixo para o exemplo
+        valorInput.sendKeys("100.0");
 
         WebElement descricaoInput = driver.findElement(By.name("description"));
-        descricaoInput.sendKeys("Transferência de teste"); // Descrição fixa para o exemplo
+        descricaoInput.sendKeys("Transferência de teste");
+
+        WebElement transferirButton = driver.findElement(By.xpath("//*[@id='__next']/div/div[3]/form/button"));
+        transferirButton.click();
+
+        WebElement mensagemSucesso = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='modalText']")));
+
+        return mensagemSucesso.getText();
+    }
+
+    public String realizarTransferenciaDadosInvalidos(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        WebElement transferenciaButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='btn-TRANSFERÊNCIA']")));
+        transferenciaButton.click();
+
+        WebElement contaInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("accountNumber")));
+        contaInput.sendKeys("9999");
+
+        WebElement digitoInput = driver.findElement(By.name("digit"));
+        digitoInput.sendKeys("9");
+
+        WebElement valorInput = driver.findElement(By.name("transferValue"));
+        valorInput.sendKeys("100.0");
+
+        WebElement descricaoInput = driver.findElement(By.name("description"));
+        descricaoInput.sendKeys("Transferência de teste conta inexistente");
+
+        WebElement transferirButton = driver.findElement(By.xpath("//*[@id='__next']/div/div[3]/form/button"));
+        transferirButton.click();
+
+        WebElement mensagemSemSucesso = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='modalText']")));
+
+        return mensagemSemSucesso.getText();
+    }
+
+    public String realizarTransferenciaSaldoMaior(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        WebElement transferenciaButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='btn-TRANSFERÊNCIA']")));
+        transferenciaButton.click();
+
+        WebElement contaInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("accountNumber")));
+        contaInput.sendKeys(this.contaDestino);
+
+        WebElement digitoInput = driver.findElement(By.name("digit"));
+        digitoInput.sendKeys(this.digitoDestino);
+
+        WebElement valorInput = driver.findElement(By.name("transferValue"));
+        valorInput.sendKeys("9900.0");
+
+        WebElement descricaoInput = driver.findElement(By.name("description"));
+        descricaoInput.sendKeys("Transferência de teste saldo maior");
 
         WebElement transferirButton = driver.findElement(By.xpath("//*[@id='__next']/div/div[3]/form/button"));
         transferirButton.click();
--- src/test/java/TransferenciaTest.java
+++ src/test/java/TransferenciaTest.java
@@ -20,6 +20,10 @@ public class TransferenciaTest extends BaseTest {
         transferencia = new Transferencia(driver);
     }
 
+    public void botaoSairPaginaPrincipal(){
+        driver.findElement(By.xpath("//*[@id='btnExit']")).click();
+    }
+
     @Test
     @Order(1)
     public void testRealizarTransferenciaComSucesso() {
@@ -28,7 +32,7 @@ public class TransferenciaTest extends BaseTest {
         cadastro.fecharModal();
         login.realizarLogin("nometeste1@gmail.com", "Senha123");
 
-        driver.findElement(By.xpath("//*[@id='btnExit']")).click();
+        botaoSairPaginaPrincipal();
         cadastro.clicarBotaoRegistrar();
         cadastro.realizarCadastro("nometeste2@gmail.com", "testeUsuario2", "Senha123");
         cadastro.fecharModal();
@@ -36,11 +40,49 @@ public class TransferenciaTest extends BaseTest {
 
         transferencia.obterDadosContaDestino();
 
-        driver.findElement(By.xpath("//*[@id='btnExit']")).click();
+        botaoSairPaginaPrincipal();
         login.realizarLogin("nometeste1@gmail.com", "Senha123");
 
         String mensagemSucesso = transferencia.realizarTransferencia();
 
         assertTrue(mensagemSucesso.contains("Transferencia realizada com sucesso"));
     }
+
+    @Test
+    @Order(2)
+    public void testFalhaTransferenciaContaInexistente(){
+        cadastro.clicarBotaoRegistrar();
+        cadastro.realizarCadastroComSaldo("nometeste1@gmail.com", "testeUsuario1", "Senha123");
+        cadastro.fecharModal();
+        login.realizarLogin("nometeste1@gmail.com", "Senha123");
+
+        String mensagemSemSucesso = transferencia.realizarTransferenciaDadosInvalidos();
+        assertTrue(mensagemSemSucesso.contains("Conta inválida ou inexistente"));
+
+    }
+
+    @Test
+    @Order(3)
+    public void testFalhaTransferenciaSaldoMaior(){
+        cadastro.clicarBotaoRegistrar();
+        cadastro.realizarCadastroComSaldo("nometeste1@gmail.com", "testeUsuario1", "Senha123");
+        cadastro.fecharModal();
+        login.realizarLogin("nometeste1@gmail.com", "Senha123");
+
+        botaoSairPaginaPrincipal();
+        cadastro.clicarBotaoRegistrar();
+        cadastro.realizarCadastro("nometeste2@gmail.com", "testeUsuario2", "Senha123");
+        cadastro.fecharModal();
+        login.realizarLogin("nometeste2@gmail.com", "Senha123");
+
+        transferencia.obterDadosContaDestino();
+
+        botaoSairPaginaPrincipal();
+        login.realizarLogin("nometeste1@gmail.com", "Senha123");
+
+        String mensagemSemSucesso = transferencia.realizarTransferenciaSaldoMaior();
+
+        assertTrue(mensagemSemSucesso.contains("Você não tem saldo suficiente para essa transação"));
+    }
+
 }
